64d2061201ac5299969d5b7b003845039ad227dc
raven-spar <ravenspar@protonmail.com>
2024-05-18 21:43:22 +0530

    db

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+Scss(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+db = SQLAlchemy(app)
+
+class Mytask(db.Model): 
+    email = db.Column(db.String(50))
+    name = db.Column(db.String(100))
+    pass_hash = db.Column(db.String(200))
+    ph_no = db.Column(db.String(13))
+    
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
